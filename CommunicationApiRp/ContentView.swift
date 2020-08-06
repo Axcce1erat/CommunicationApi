9607df0e1c7aae0b9aaeaacda3c23c1bbbefe266
Axel Schwarz <a.schwarz@telekom.de>
2020-08-06 16:44:27 +0200

    First commit

---
+++ CommunicationApiRp/ContentView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+import SwifteriOS
+
+public struct ContentView: View {
+    var swifter = Swifter(consumerKey: "E5PXkHjR3RWabtdyufbqIlgsE", consumerSecret: "Wp6YO6VMxhH6ZxJQvPj91SYekoT9TgpKqyH3oyH41Xjfw7Hxsd")
+    public var body: some View {
+        Text("Hi")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
